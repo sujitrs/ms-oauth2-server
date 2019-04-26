9e89206fe75fe96ceb36aa24a2e550bffae555a6
sujitrs <suryawanshisujeet@gmail.com>
2019-04-26 15:58:05 +0530

    Working Copy : testes with Oauth-ms-user-repo and oauth-ms-config-server

---
+++ src/main/java/org/sj/msoauth2server/MsOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package org.sj.msoauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOauth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOauth2ServerApplication.class, args);
+	}
+
+}
