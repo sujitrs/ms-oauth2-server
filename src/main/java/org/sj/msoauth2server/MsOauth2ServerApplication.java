f35a9b5c735679a4b0648996bbf520434b588f66
sujitrs <suryawanshisujeet@gmail.com>
2019-05-27 19:03:20 +0530

    WORKING_COPY

--- src/main/java/org/sj/msoauth2server/MsOauth2ServerApplication.java
+++ src/main/java/org/sj/msoauth2server/MsOauth2ServerApplication.java
@@ -2,11 +2,13 @@ package org.sj.msoauth2server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableDiscoveryClient
 public class MsOauth2ServerApplication {
 
 	public static void main(String[] args) {
